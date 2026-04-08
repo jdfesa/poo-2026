fdb1094e6958a932a1f8d0209cdec29be579451f
jdfesa <asefadoj@gmail.com>
2026-04-07 21:33:05 -0300

    feat(unsa) add project ArraysCodingChallenge

---
+++ UNSa/desafio_repaso/ArraysCodingChallenge/src/main/java/ar/edu/unsa/arrayscodingchallenge/ArraysCodingChallenge.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ar.edu.unsa.arrayscodingchallenge;
+
+/**
+ *
+ * @author jd
+ */
+public class ArraysCodingChallenge {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
